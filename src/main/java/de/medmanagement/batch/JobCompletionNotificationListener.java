215a7cc1a27699d72a9a894df5b86ea20e69202c
borowski <christoph.borowski@itemis.de>
2021-01-27 10:10:24 +0100

    System.out.println ("...") commented out. Removed unnecessary code.

--- src/main/java/de/medmanagement/batch/JobCompletionNotificationListener.java
+++ src/main/java/de/medmanagement/batch/JobCompletionNotificationListener.java
@@ -8,23 +8,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
 
-    // private final JdbcTemplate jdbcTemplate;
-
-    /*
-    @Autowired
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-    */
-
     @Override
     public void afterJob(JobExecution jobExecution) {
         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-            System.out.println("BATCH JOB COMPLETED SUCCESSFULLY");
-
-            // String query = "SELECT brand, origin, characteristics FROM coffee";
-            // jdbcTemplate.query(query, (rs, row) -> new Coffee(rs.getString(1), rs.getString(2), rs.getString(3)))
-            //         .forEach(coffee -> LOGGER.info("Found < {} > in the database.", coffee));
+            // System.out.println("BATCH JOB COMPLETED SUCCESSFULLY");
         }
     }
 }
